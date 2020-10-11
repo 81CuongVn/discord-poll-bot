0365b45eee1a1e07506babd72a3e8a8423784249
ItzColtz <65344522+ItzColtz@users.noreply.github.com>
2020-10-11 10:31:42 +0100

    poll help command

---
+++ src/commands/help.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+    name: 'help',
+    execute(message) {
+        const embed = {
+            color: 'BLUE',
+            thumbnail: {
+                url: message.client.user.displayAvatarURL({ format: 'png' }),
+            },
+            description: `
+                **Polls**
+                Create a poll using the command
+                \`${process.env.prefix}poll {Title} [Option 1] [Option 2] [Option 3]\`
+
+                You can create a poll with a expiry date and end the poll once over
+                \`${process.env.prefix}poll {Title} {Time} [Option 1] [Option 2] [Option 3]\`
+
+                **Note:** Each poll can have up to 20 options.
+
+                **Other Commands**
+                ${message.client.commands.filter(c => c.name != 'poll').map(c => `\`${c.name}\``).join(' ')}
+            `,
+        };
+
+        message.channel.send({ embed: embed });
+    },
+};
